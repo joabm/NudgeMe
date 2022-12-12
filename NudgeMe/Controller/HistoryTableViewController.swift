c7b3bd785e8c1957ddb674c1a765a2896b194be8
joabm <84096469+joabm@users.noreply.github.com>
2022-12-11 18:27:43 -0800

    added HistoryTableViewController file

---
+++ NudgeMe/Controller/HistoryTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  HistoryTableViewController.swift
+//  NudgeMe
+//
+//  Created by Joab Maldonado on 12/11/22.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class HistoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var createdDateLabel: UILabel!
+    
+}
+
+class HistoryTableViewController: UITableViewController, NSFetchedResultsControllerDelegate {
+    
+    @IBAction func clearHistoryButton(_ sender: Any) {
+        deleteStoreReminders()
+        //tableView.reloadData()
+    }
+    
+    
+    /// A date formatter for date text in note cells
+    let dateFormatter: DateFormatter = {
+        let df = DateFormatter()
+        df.dateStyle = .medium
+        return df
+    }()
+    
+    // MARK: CoreData
+    //access the reminder array in CoreData
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var message: Message!
+    var fetchedResultsController:NSFetchedResultsController<Message>!
+    
+    fileprivate func setupFetchedResultsController() {
+        let fetchRequest:NSFetchRequest<Message> = Message.fetchRequest()
+        let predicate = NSPredicate(format: "message == %@", message)
+        fetchRequest.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        fetchedResultsController.delegate = self
+
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    
+    func deleteStoreReminders() {
+        for message in fetchedResultsController.fetchedObjects! {
+            context.delete(message)
+        }
+        try? context.save()
+    }
+    
+    func deleteMessage(at indexPath: IndexPath) {
+        let messageToDelete = fetchedResultsController.object(at: indexPath)
+        context.delete(messageToDelete)
+        try? context.save()
+    }
+    
+    // MARK: Lifecycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let indexPath = tableView.indexPathForSelectedRow {
+            tableView.deselectRow(at: indexPath, animated: false)
+            tableView.reloadRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupFetchedResultsController()
+        
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        fetchedResultsController = nil
+    }
+    
+    // MARK: Table View Data Source
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //fetch reminder count from core data
+        return fetchedResultsController.sections?.count ?? 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        //get reminder data from core data and display
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReminderCell", for: indexPath) as! HistoryTableViewCell
+        let aMessage = fetchedResultsController.object(at: indexPath)
+        
+        //Set the text
+        cell.messageLabel.text = aMessage.text
+        
+        if let creationDate = aMessage.creationDate {
+            cell.createdDateLabel.text = dateFormatter.string(from: creationDate)
+        }
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            deleteMessage(at: indexPath)
+            //tableView.reloadData()
+        }
+    }
+    
+    
+}

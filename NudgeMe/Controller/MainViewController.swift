2036e2b8aba3a16f99410170d8a8bef2121a90b0
joabm <84096469+joabm@users.noreply.github.com>
2022-12-11 23:16:54 -0800

    cleaned up main view controller

--- NudgeMe/Controller/MainViewController.swift
+++ NudgeMe/Controller/MainViewController.swift
@@ -20,22 +20,8 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         notifications.requestNotificationAuthorization()
         manager.requestLocationAuthorization()
-//        repeat24HrsTimer()
 
     }
     
-    func repeat24HrsTimer() {
-        let date = Date(timeIntervalSinceReferenceDate: 60)
-        let timer = Timer(fireAt: date, interval: 7200, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
-        RunLoop.main.add(timer, forMode: .common)
-    }
-
-    @objc func startTimer(){
-        notifications.center.removeAllPendingNotificationRequests()
-        intervals.getCategoryArrays()
-        intervals.getRandomReminders()
-        intervals.scheduleReminders()
-    }
-
 }
 
